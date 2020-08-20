2d61ca3198be9eb945930f7bf6386aaa84ff2fed
wiiinnaa <wiiinnaa@gmail.com>
2020-08-19 22:14:49 -0500

    cabezera y pie de pagina

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sistema Wisp</title>
+        <!--BOOTSTRAP 4 -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- FONT AWESOEM -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/all.min.css">
+    </head>
+    <body>
+        <nav class= "navbar navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href= "index.php">Sistema de pagos WISP V. 1</a><pre></pre>
+            </div>
+        </nav>
